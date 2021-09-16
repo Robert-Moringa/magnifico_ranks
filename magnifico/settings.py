386542c15a8030552a31a9c6407e47a3203110c5
Robert Maina <robert.maina@student.moringaschool.com>
2021-09-16 21:04:06 +0300

    added bootstrap and magnifico apps in the settings.py

--- magnifico/settings.py
+++ magnifico/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap5',
+    'magnifico_ranks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
