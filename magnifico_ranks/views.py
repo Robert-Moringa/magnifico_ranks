7c9128e7a3fa40c2d5f931bbbc1d51930e4ac896
Robert Maina <robert.maina@student.moringaschool.com>
2021-09-19 12:43:28 +0300

    added a project and displayed it in the template

--- magnifico_ranks/views.py
+++ magnifico_ranks/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
 def home(request):
+    projects=Project.objects.all()
     title='Welcome in a array of superb projects that are awaiting your genuine vote.'
-    return render(request, 'index.html', {'title':title,})
+    return render(request, 'index.html', {'title':title, 'projects':projects})
