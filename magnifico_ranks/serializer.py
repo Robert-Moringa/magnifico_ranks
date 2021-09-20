cbca3689e6a8ce5de1773a6bd3454203fc6a180c
Robert Maina <robert.maina@student.moringaschool.com>
2021-09-20 05:28:42 +0300

    added authentification for the post method

--- magnifico_ranks/serializer.py
+++ magnifico_ranks/serializer.py
@@ -5,3 +5,8 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ('id', 'user', 'bio','profile_pic', 'contact','project')
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ('id', 'name', 'project_image','about', 'project_link', 'user')
--- magnifico_ranks/urls.py
+++ magnifico_ranks/urls.py
@@ -7,7 +7,8 @@ urlpatterns=[
     url('^$',views.home,name = 'home'),
     url(r'^profile/user$', views.profile, name='profile'),
     url(r'^api/profile/$', views.profileList.as_view()),
-    url(r'api/profile/profile-id/(?P<pk>[0-9]+)/$',views.ProfileDescription.as_view())
+    url(r'api/profile/profile-id/(?P<pk>[0-9]+)/$',views.ProfileDescription.as_view()),
+    url(r'^api/profile/$', views.projectList.as_view()),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
